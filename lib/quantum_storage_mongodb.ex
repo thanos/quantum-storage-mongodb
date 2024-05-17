9e92b4ee2a2b324e106d3dc0f74fd6e305655127
thanos <thanosv@gmail.com>
2024-05-17 00:43:52 -0400

    completed code to pass unittests

--- lib/quantum_storage_mongodb.ex
+++ lib/quantum_storage_mongodb.ex
@@ -9,27 +9,15 @@ defmodule QuantumStorageMongodb do
 
   @behaviour Quantum.Storage
 
+  #
+  #
+  # API
+  #
+  #
+
   @doc false
   def start_link(opts),
     do: GenServer.start_link(__MODULE__, opts, opts)
-
-  @doc false
-  @impl GenServer
-  def init(opts) do
-    {:ok, conn} =
-      Mongo.start_link(
-        url: opts |> Keyword.fetch!(:url)
-        # username: opts |> Keyword.fetch!(:username),
-        # password: opts |> Keyword.fetch!(:password),
-        # auth_source: opts |> Keyword.fetch!(:auth_source)
-      )
-
-    {:ok,
-     %{
-       db: conn,
-       collection: opts |> Keyword.fetch!(:collection)
-     }}
-  end
 
   @doc false
   @impl Quantum.Storage
@@ -38,8 +26,6 @@ defmodule QuantumStorageMongodb do
   @doc false
   @impl Quantum.Storage
   def add_job(storage_pid, job) do
-    Logger.debug("#{inspect({storage_pid, {:add_job, job}})}")
-    IO.inspect({storage_pid, {:add_job, job}})
     GenServer.cast(storage_pid, {:add_job, job})
   end
 
@@ -65,42 +51,107 @@ defmodule QuantumStorageMongodb do
   @impl Quantum.Storage
   def purge(storage_pid), do: GenServer.cast(storage_pid, :purge)
 
+  #
+  #
+  # SERVER
+  #
+  #
+
+  @doc false
+  @impl GenServer
+  def init(opts) do
+    {:ok, conn} =
+      Mongo.start_link(
+        url: opts |> Keyword.fetch!(:url)
+        # username: opts |> Keyword.fetch!(:username),
+        # password: opts |> Keyword.fetch!(:password),
+        # auth_source: opts |> Keyword.fetch!(:auth_source)
+      )
+
+    {:ok,
+     %{
+       db: conn,
+       collection: opts |> Keyword.fetch!(:collection)
+     }}
+  end
+
   @doc false
   @impl GenServer
   def handle_cast({:add_job, job}, %{db: db, collection: collection} = state) do
-    Logger.debug("#{inspect({:add_job, job})}")
-    IO.inspect({:add_job, job})
-    Mongo.insert_one(db, collection, %{"_id" => job.name, "job" => job})
-    {:noreply, state}
-  end
+    Logger.debug("handle_cast #{inspect({:add_job, job})}")
+    marked_used(db, collection)
 
-  def handle_cast({:delete_job, job_name}, %{db: db, collection: collection} = state) do
-    Mongo.delete_one(db, collection, %{"_id" => job_name})
-    {:noreply, state}
-  end
-
-  def handle_cast(
-        {:update_job_state, job_name, job_state},
-        %{db: db, collection: collection} = state
-      ) do
-    Mongo.update_one(db, collection, %{"_id" => job_name}, %{"job" => job_state})
-    {:noreply, state}
-  end
-
-  def handle_cast(
-        {:update_last_execution_date, last_execution_date},
-        %{db: db, collection: collection} = state
-      ) do
-    Mongo.update_one(db, collection, %{"_id" => "last_execution_date"}, %{
-      "date" => last_execution_date
+    Mongo.insert_one(db, collection, %{
+      "_id" => encode_name(job.name),
+      "job" => encode_job(job),
+      "type" => "data"
     })
 
     {:noreply, state}
   end
 
+  @doc false
+  @impl GenServer
+  def handle_cast({:delete_job, job_name}, %{db: db, collection: collection} = state) do
+    Mongo.delete_one(db, collection, %{"_id" => encode_name(job_name)})
+    {:noreply, state}
+  end
+
+  @doc false
+  @impl GenServer
+  def handle_cast(
+        {:update_job_state, job_name, job_state},
+        %{db: db, collection: collection} = state
+      ) do
+    %{"_id" => id, "job" => job} =
+      Mongo.find_one(db, collection, %{"_id" => encode_name(job_name)})
+
+    Mongo.update_one(db, collection, %{"_id" => id}, %{
+      "$set" => %{
+        "job" =>
+          job
+          |> decode_job()
+          |> Quantum.Job.set_state(job_state)
+          |> encode_job()
+      }
+    })
+
+    {:noreply, state}
+  end
+
+  @doc false
+  @impl GenServer
   def handle_cast(:purge, %{db: db, collection: collection} = state) do
     Mongo.delete_many(db, collection, %{})
     {:noreply, state}
+  end
+
+  @doc false
+  @impl GenServer
+  def handle_cast(
+        {:update_last_execution_date, last_execution_date},
+        %{db: db, collection: collection} = state
+      ) do
+    update = %{
+      "date" => last_execution_date |> encode_date,
+      "type" => "bookkeeping"
+    }
+
+    Mongo.replace_one(db, collection, %{"_id" => "last_execution_date"}, update, upsert: true)
+    {:noreply, state}
+  end
+
+  @doc false
+  @impl GenServer
+  def handle_call(:last_execution_date, _from, %{db: db, collection: collection} = state) do
+    # Mongo.find(db, collection, %{})
+    # |> IO.inspect(label: "before")
+    {:reply,
+     Mongo.find_one(db, collection, %{"_id" => "last_execution_date"})
+     |> case do
+       nil -> :unknown
+       %{"date" => date} -> date |> decode_date
+     end, state}
   end
 
   @doc false
@@ -110,20 +161,36 @@ defmodule QuantumStorageMongodb do
      Mongo.find(db, collection, %{})
      |> Enum.to_list()
      |> case do
+       [
+         %{
+           "_id" => "used",
+           "state" => true
+         }
+       ] ->
+         []
+
        [] ->
          :not_applicable
 
        jobs ->
          jobs
+         |> Enum.reject(fn %{"type" => type} -> type == "bookkeeping" end)
+         |> Enum.map(fn %{"job" => job} -> job |> decode_job end)
      end, state}
   end
 
-  def handle_call(:last_execution_date, _from, %{db: db, collection: collection} = state) do
-    {:reply,
-     Mongo.find_one(db, collection, %{"id" => "last_execution_date"})
-     |> case do
-       nil -> :unknown
-       %{"date" => date} -> date
-     end, state}
+  def marked_used(db, collection) do
+    Mongo.insert_one(db, collection, %{
+      "_id" => "used",
+      "state" => true,
+      "type" => "bookkeeping"
+    })
   end
+
+  def encode_job(job), do: :erlang.term_to_binary(job)
+  def decode_job(job), do: :erlang.binary_to_term(job)
+  def encode_name(name), do: :erlang.term_to_binary(name)
+  def decode_name(name), do: :erlang.binary_to_term(name)
+  def encode_date(date), do: :erlang.term_to_binary(date)
+  def decode_date(date), do: :erlang.binary_to_term(date)
 end
--- test/quantum_storage_mongodb_test.exs
+++ test/quantum_storage_mongodb_test.exs
@@ -22,18 +22,77 @@ defmodule QuantumStorageMongodbTest do
     {:ok, storage: storage}
   end
 
+  describe "purge/1" do
+    test "purges correct module", %{storage: storage} do
+      assert :ok = QuantumStorageMongodb.add_job(storage, Scheduler.new_job())
+      assert :ok = QuantumStorageMongodb.purge(storage)
+      assert :not_applicable = QuantumStorageMongodb.jobs(storage)
+    end
+  end
+
   describe "add_job/2" do
     test "adds job", %{storage: storage} do
-      Mongo
-      |> stub(:insert_one, fn _db, _col, _obj -> :stub end)
-      |> expect(:insert_one, fn db, col, obj ->
-        IO.inspect({db, col, obj})
-        :ok
-      end)
+      # Mongo
+      # |> stub(:insert_one, fn _db, _col, _obj -> :stub end)
+      # |> expect(:insert_one, fn db, col, obj ->
+      #   IO.inspect({db, col, obj})
+      #   :ok
+      # end)
 
       job = Scheduler.new_job()
       assert :ok = QuantumStorageMongodb.add_job(storage, job)
-      # assert [^job] = QuantumStorageMongodb.jobs(storage)
+      assert [^job] = QuantumStorageMongodb.jobs(storage)
+    end
+  end
+
+  describe "delete_job/2" do
+    test "deletes job", %{storage: storage} do
+      job = Scheduler.new_job()
+      assert :ok = QuantumStorageMongodb.add_job(storage, job)
+      assert :ok = QuantumStorageMongodb.delete_job(storage, job.name)
+      assert [] = QuantumStorageMongodb.jobs(storage)
+    end
+
+    test "does not fail when deleting unknown job", %{storage: storage} do
+      job = Scheduler.new_job()
+      assert :ok = QuantumStorageMongodb.add_job(storage, job)
+      assert :ok = QuantumStorageMongodb.delete_job(storage, make_ref())
+    end
+  end
+
+  describe "update_job_state/2" do
+    test "updates job", %{storage: storage} do
+      job = Scheduler.new_job()
+      assert :ok = QuantumStorageMongodb.add_job(storage, job)
+      assert :ok = QuantumStorageMongodb.update_job_state(storage, job.name, :inactive)
+      assert [%{state: :inactive}] = QuantumStorageMongodb.jobs(storage)
+    end
+
+    test "does not fail when updating unknown job", %{storage: storage} do
+      job = Scheduler.new_job()
+      assert :ok = QuantumStorageMongodb.add_job(storage, job)
+
+      assert :ok = QuantumStorageMongodb.update_job_state(storage, make_ref(), :inactive)
+    end
+  end
+
+  describe "update_last_execution_date/2" do
+    test "sets time on scheduler", %{storage: storage} do
+      date = NaiveDateTime.utc_now()
+      assert :ok = QuantumStorageMongodb.update_last_execution_date(storage, date)
+      assert ^date = QuantumStorageMongodb.last_execution_date(storage)
+    end
+  end
+
+  describe "last_execution_date/1" do
+    test "gets time", %{storage: storage} do
+      date = NaiveDateTime.utc_now()
+      assert :ok = QuantumStorageMongodb.update_last_execution_date(storage, date)
+      assert ^date = QuantumStorageMongodb.last_execution_date(storage)
+    end
+
+    test "get unknown otherwise", %{storage: storage} do
+      assert :unknown = QuantumStorageMongodb.last_execution_date(storage)
     end
   end
 end
